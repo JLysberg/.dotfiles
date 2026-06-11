44deac8cb11f153a6c6c01a119e8225d7a1c1c66
JLysberg <jostein.lysberg@gmail.com>
2026-06-11 11:52:38 +0200

    feat(nvim): always show bufferline

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -2,10 +2,16 @@ return {
   {
     "akinsho/bufferline.nvim",
     opts = function(_, opts)
-      opts.options = opts.options or {}
-      opts.options.custom_filter = function(buf)
-        return vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].modified
+      local function buffer_is_modified(buf)
+        return vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].buflisted and vim.bo[buf].modified
       end
+
+      opts.options = opts.options or {}
+      opts.options.always_show_bufferline = true
+      opts.options.custom_filter = function(buf)
+        return buffer_is_modified(buf)
+      end
+
     end,
   },
 }
