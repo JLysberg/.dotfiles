153ee359c9486d829615da3e7cddbe27aa6d8913
JLysberg <jostein.lysberg@gmail.com>
2026-07-29 09:37:06 +0200

    feat(nvim): configure scroll animations and cursor smearing

--- nvim/lua/plugins/snacks-animated-scrolling-off.lua
+++ nvim/lua/plugins/snacks-animated-scrolling-off.lua
@@ -1,24 +1,28 @@
 return {
-	"folke/snacks.nvim",
-	opts = {
-		picker = {
-			win = {
-				input = {
-					keys = {
-						["<c-d>"] = { "preview_scroll_down", mode = { "i", "n" } },
-						["<c-u>"] = { "preview_scroll_up", mode = { "i", "n" } },
-					},
-				},
-				list = {
-					keys = {
-						["<c-d>"] = "preview_scroll_down",
-						["<c-u>"] = "preview_scroll_up",
-					},
-				},
-			},
-		},
-		scroll = {
-			enabled = false, -- Disable scrolling animations
-		},
-	},
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      win = {
+        input = {
+          keys = {
+            ["<c-d>"] = { "preview_scroll_down", mode = { "i", "n" } },
+            ["<c-u>"] = { "preview_scroll_up", mode = { "i", "n" } },
+          },
+        },
+        list = {
+          keys = {
+            ["<c-d>"] = "preview_scroll_down",
+            ["<c-u>"] = "preview_scroll_up",
+          },
+        },
+      },
+    },
+    scroll = {
+      enabled = true,
+      animate = {
+        duration = { step = 10, total = 30 },
+        easing = "outQuad",
+      },
+    },
+  },
 }
